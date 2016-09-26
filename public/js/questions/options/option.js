9b1ad296e9198fdf93def47f8a6b814f04d74cd4
tzachi.h <tzachi.h@convertmedia.com>
2016-09-26 19:39:40 +0300

    fix installation and editor bugs

--- public/js/questions/options/option.js
+++ public/js/questions/options/option.js
@@ -1,31 +1,216 @@
-function showOption(questionUid, optionUid){
-   console.log("showOption: "+questionUid, optionUid)
-   //show header
-   DB.child("questions/"+questionUid+"/options/"+optionUid+"/title").once("value", function(titleInDB){
-      renderTemplate("#optionHeaderTitle-tmpl", {title:titleInDB.val()}, "#headerTitle")
-   })
+//function showOption(questionUid, optionUid){
+//   console.log("showOption: "+questionUid, optionUid)
+//   //show header
+//   DB.child("questions/"+questionUid+"/options/"+optionUid+"/title").once("value", function(titleInDB){
+//      renderTemplate("#optionHeaderTitle-tmpl", {title:titleInDB.val()}, "#headerTitle")
+//   })
+//
+//   renderTemplate("#headerMenu-tmpl", {chatUid: optionUid, entityType: "options"}, "#headerMenu");
+//
+//   //show footer
+//   renderTemplate("#optionFooter-tmpl", {questionUid:questionUid}, "footer");
+//
+//   //show wrapper
+//   DB.child("questions/"+questionUid+"/options/"+optionUid).once("value", function(dataSnapshot){
+//
+//      var description = dataSnapshot.val().description;
+//
+//      renderTemplate("#optionWrapper-tmpl", {questionUid:questionUid, optionUid:optionUid, description:description}, "wrapper")
+//   });
+//
+//   $("#"+optionUid+"DivOpt").keyup(function(e) {
+//      //
+//      var description = $("#"+optionUid+"DivOpt").html();
+//      console.log("text: "+description);
+//
+//
+//      DB.child("questions/"+questionUid+"/options/"+optionUid).update({description:description, writer: userUuid});
+//   });
+//
+//   //update wrapper
+//
+//   var updateWrapperOption = function(optionDB){
+//
+//      var descriptionFromDB = optionDB.val().description;
+//      var writer = optionDB.val().writer;
+//
+//      if (writer != userUuid){
+//         console.log("write")
+//         $("#"+optionUid+"DivOpt").html(descriptionFromDB);
+//      }
+//   }
+//
+//   DB.child("questions/"+questionUid+"/options/"+optionUid).on("value", updateWrapperOption);
+//
+//   var turnOff = function () {
+//      DB.child("questions/"+questionUid+"/options/"+optionUid).off("value", updateWrapperOption);
+//   };
+//
+//
+//   //setActive entity
+//   DB.child("questions/"+questionUid+"/options/"+optionUid).once("value", function(dataSnapshot){
+//      var optionKey = dataSnapshot.val().optionKey;
+//      setActiveEntity("options", optionKey, "value", updateWrapperOption, turnOff);
+//   })
+//
+//
+//}
+//
+//function updateOptionDescription(questionUid, optionUid){
+//   var description = $("#"+optionUid+"DivOpt").text();
+//   console.log(this, questionUid, optionUid, description);
+//   DB.child("questions/"+questionUid+"/options/"+optionUid).update({description:description, writer: userUuid});
+//}
+//
+////create option
+//function createOption(questionUid, title, description, explanation){
+//
+//   var color = getRandomColor();
+//
+//   if(description == "" || description == undefined){
+//      description = "";
+//   }
+//   if(explanation == "" || explanation == undefined){
+//      explanation = "";
+//   }
+//
+//   if(title == "" || title == undefined){
+//      console.log("Eror: title = "+ title);
+//   } else {
+//      var optionUidDB = DB.child("questions/"+questionUid+"/options").push({title: title, description: description, explanation: explanation, color:color, ownerUid: userUuid});
+//
+//      var optionKeyDB = DB.child("options").push({questionUid: questionUid, optionUid: optionUidDB.key});
+//
+//      DB.child("questions/"+questionUid+"/options/"+optionUidDB.key).update({optionKey: optionKeyDB.key});
+//   }
+//}
+//
+//function openOptionMenu(optionUid){
+//   if ($("#optionMenu"+optionUid).is(":visible")){
+//      $("#optionMenu"+optionUid).hide(400);
+//   } else {
+//      $("#optionMenu"+optionUid).show(400);
+//   }
+//
+//}
+//
+//function editOption(questionUid, optionUid){
+//
+//   renderTemplate("#editMultiOptionFooter-tmpl", {questionUid:questionUid, optionUid: optionUid}, "footer");
+//   renderTemplate("#createMultiOption-tmpl", {}, "wrapper");
+//
+//   DB.child("questions/"+questionUid+"/options/"+optionUid).once("value", function(dataSnapshot){
+//      var title = dataSnapshot.val().title;
+//      var description = dataSnapshot.val().description;
+//      $("#createMultiOptionName").val(title);
+//      $("#createMultiOptionDescription").text(description);
+//   })
+//
+//
+//}
+//
+//function editMultiOptionToDB (questionUid, optionUid){
+//
+//   var title =  $("#createMultiOptionName").val();
+//   var description = $("#createMultiOptionDescription").val();
+//
+//   console.log(title, description);
+//   DB.child("questions/"+questionUid+"/options/"+optionUid).update({title: title, description:description});
+//
+//   showMultiOptions(questionUid);
+//}
+//
+////show option info in limited options
+//
+//function showOptionInfo(question, option){
+//
+//   if ($("#info").is(":visible")){
+//      $("#info").hide(400);
+//   } else{
+//
+//      DB.child("questions/"+question+"/options/"+option).once("value", function(dataSnapshot){
+//
+//         var title = dataSnapshot.val().title;
+//         var description = dataSnapshot.val().description;
+//         var explanation = dataSnapshot.val().explanation;
+//
+//         var context = {title: title, description: description, explanation: explanation}
+//
+//         renderTemplate("#optionsInfo-tmpl", context, "#info");
+//
+//         //get wrapper dimentions
+//         var headerHeight = $("header").height();
+//         var wrapperHeight = $("wrapper").height();
+//         var footerHeight = $("footer").height();
+//         var infoHeight = wrapperHeight-footerHeight;
+//         var headerWidth = $("header").width();
+//
+//         $("#info").css("top", headerHeight).css("height", infoHeight).css("width", headerWidth).show(400);
+//
+//      })
+//
+//
+//   }
+//}
 
-   renderTemplate("#headerMenu-tmpl", {chatUid: optionUid, entityType: "options"}, "#headerMenu");
 
-   //show footer
-   renderTemplate("#optionFooter-tmpl", {questionUid:questionUid}, "footer");
+
+function showOption(questionUid, optionUid) {
+    console.log("showOption: " + questionUid, optionUid)
+        //show header
+    DB.child("questions/" + questionUid + "/options/" + optionUid + "/title").once("value", function (titleInDB) {
+        renderTemplate("#optionHeaderTitle-tmpl", {
+            title: titleInDB.val()
+        }, "#headerTitle")
+    })
+
+    renderTemplate("#headerMenu-tmpl", {
+        chatUid: optionUid,
+        entityType: "options"
+    }, "#headerMenu");
+
+    //show footer
+    renderTemplate("#optionFooter-tmpl", {
+        questionUid: questionUid
+    }, "footer");
 
    //show wrapper
    DB.child("questions/"+questionUid+"/options/"+optionUid).once("value", function(dataSnapshot){
 
-      var description = dataSnapshot.val().description;
+        var description = dataSnapshot.val().description;
 
-      renderTemplate("#optionWrapper-tmpl", {questionUid:questionUid, optionUid:optionUid, description:description}, "wrapper")
-   });
+        renderTemplate("#optionWrapper-tmpl", {
+            questionUid: questionUid,
+            optionUid: optionUid,
+            description: description
+        }, "wrapper");
+//        $('wrapper').html("<div id='" + optionUid + "DivOpt' class='card' contenteditable='true' >"+description+"</div>")
+        $('wrapper').html("<div id='"+optionUid+"DivOpt' class='card'>"+description+"</div>")
+
+        createQuillEditor("#" + optionUid + "DivOpt");
+
+    });
 
    $("#"+optionUid+"DivOpt").keyup(function(e) {
       //
       var description = $("#"+optionUid+"DivOpt").html();
       console.log("text: "+description);
 
+        DB.child("questions/" + questionUid + "/options/" + optionUid).update({
+            description: description,
+            writer: userUuid
+        });
+    });
 
-      DB.child("questions/"+questionUid+"/options/"+optionUid).update({description:description, writer: userUuid});
-   });
+    $(".ql-toolbar span").click(function () {
+        var description = $("#" + optionUid + "DivOpt").html();
+        console.log("\n\n-Toolbar click: " + description);
+
+        DB.child("questions/" + questionUid + "/options/" + optionUid).update({
+            description: description,
+            writer: userUuid
+        });
+    });
 
    //update wrapper
 
@@ -34,11 +219,11 @@ function showOption(questionUid, optionUid){
       var descriptionFromDB = optionDB.val().description;
       var writer = optionDB.val().writer;
 
-      if (writer != userUuid){
-         console.log("write")
-         $("#"+optionUid+"DivOpt").html(descriptionFromDB);
-      }
-   }
+        if (writer != userUuid) {
+            console.log("write");
+            $("#" + optionUid + "DivOpt").html(descriptionFromDB);
+        }
+    }
 
    DB.child("questions/"+questionUid+"/options/"+optionUid).on("value", updateWrapperOption);
 
@@ -56,10 +241,13 @@ function showOption(questionUid, optionUid){
 
 }
 
-function updateOptionDescription(questionUid, optionUid){
-   var description = $("#"+optionUid+"DivOpt").text();
-   console.log(this, questionUid, optionUid, description);
-   DB.child("questions/"+questionUid+"/options/"+optionUid).update({description:description, writer: userUuid});
+function updateOptionDescription(questionUid, optionUid) {
+    var description = $("#" + optionUid + "DivOpt").text();
+    console.log(this, questionUid, optionUid, description);
+    DB.child("questions/" + questionUid + "/options/" + optionUid).update({
+        description: description,
+        writer: userUuid
+    });
 }
 
 //create option
@@ -74,15 +262,26 @@ function createOption(questionUid, title, description, explanation){
       explanation = "";
    }
 
-   if(title == "" || title == undefined){
-      console.log("Eror: title = "+ title);
-   } else {
-      var optionUidDB = DB.child("questions/"+questionUid+"/options").push({title: title, description: description, explanation: explanation, color:color, ownerUid: userUuid});
+    if (title == "" || title == undefined) {
+        console.log("Eror: title = " + title);
+    } else {
+        var optionUidDB = DB.child("questions/" + questionUid + "/options").push({
+            title: title,
+            description: description,
+            explanation: explanation,
+            color: color,
+            ownerUid: userUuid
+        });
 
-      var optionKeyDB = DB.child("options").push({questionUid: questionUid, optionUid: optionUidDB.key});
+        var optionKeyDB = DB.child("options").push({
+            questionUid: questionUid,
+            optionUid: optionUidDB.key
+        });
 
-      DB.child("questions/"+questionUid+"/options/"+optionUidDB.key).update({optionKey: optionKeyDB.key});
-   }
+        DB.child("questions/" + questionUid + "/options/" + optionUidDB.key).update({
+            optionKey: optionKeyDB.key
+        });
+    }
 }
 
 function openOptionMenu(optionUid){
@@ -96,8 +295,11 @@ function openOptionMenu(optionUid){
 
 function editOption(questionUid, optionUid){
 
-   renderTemplate("#editMultiOptionFooter-tmpl", {questionUid:questionUid, optionUid: optionUid}, "footer");
-   renderTemplate("#createMultiOption-tmpl", {}, "wrapper");
+    renderTemplate("#editMultiOptionFooter-tmpl", {
+        questionUid: questionUid,
+        optionUid: optionUid
+    }, "footer");
+    renderTemplate("#createMultiOption-tmpl", {}, "wrapper");
 
    DB.child("questions/"+questionUid+"/options/"+optionUid).once("value", function(dataSnapshot){
       var title = dataSnapshot.val().title;
@@ -114,8 +316,11 @@ function editMultiOptionToDB (questionUid, optionUid){
    var title =  $("#createMultiOptionName").val();
    var description = $("#createMultiOptionDescription").val();
 
-   console.log(title, description);
-   DB.child("questions/"+questionUid+"/options/"+optionUid).update({title: title, description:description});
+    console.log(title, description);
+    DB.child("questions/" + questionUid + "/options/" + optionUid).update({
+        title: title,
+        description: description
+    });
 
    showMultiOptions(questionUid);
 }
@@ -134,7 +339,11 @@ function showOptionInfo(question, option){
          var description = dataSnapshot.val().description;
          var explanation = dataSnapshot.val().explanation;
 
-         var context = {title: title, description: description, explanation: explanation}
+            var context = {
+                title: title,
+                description: description,
+                explanation: explanation
+            }
 
          renderTemplate("#optionsInfo-tmpl", context, "#info");
 
