dafee620d7ead02d192e5374092130d40aaebc6d
Tal Yaron <tal.yaron@gmail.com>
2016-09-28 18:47:57 +0300

    chats symbol

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,36 @@
 $(function(){
 
-   //start ripple effect
-   $(".footer-btn").ePulse({
+  //start ripple effect
+  $(".footer-btn").ePulse({
 
-      bgColor: "#ded9d9",
-      size: 'medium'
+    bgColor: "#ded9d9",
+    size: 'medium'
 
-   });
+  });
 
-   $(".headerMenuBtn").ePulse({
-      bgColor: "#ded9d9",
-      size: 'medium'
-   });
+  $(".headerMenuBtn").ePulse({
+    bgColor: "#ded9d9",
+    size: 'medium'
+  });
 
-   var currentUrl = getUrl();
+  var currentUrl = getUrl();
 
-   startingUrl = currentUrl;
-//   routTo(currentUrl);
+  startingUrl = currentUrl;
+  //   routTo(currentUrl);
 
-   listenToAuth(currentUrl);
+  listenToAuth(currentUrl);
 
 
-   $(window).on('popstate', function() {
+  $(window).on('popstate', function() {
 
-      console.log("rout to.. popstate");
-      var currentUrl = getUrl();
-      var back = true;
-      routTo(currentUrl,back);
-      
-      window.scrollTo(0,1);
+    console.log("rout to.. popstate");
+    var currentUrl = getUrl();
+    var back = true;
+    routTo(currentUrl,back);
 
-   });
+    window.scrollTo(0,1);
+
+  });
 });
 
 
@@ -45,19 +45,19 @@ var activeEntity = new Object();
 var firstRun = true;
 
 const subEntity = {
-   groups: "topics"
-   , topics: "questions"
-   , questions: "options"
-   , chat: "room"
-   , liveTalks: "live Talk"
+  groups: "topics"
+  , topics: "questions"
+  , questions: "options"
+  , chat: "room"
+  , liveTalks: "live Talk"
 };
 const toHebrew = {
-   groups: "קבוצה חדשה: "
-   , topics: "נושא חדש: "
-   , questions: "שאלה חדשה: "
-   , owner: "קריאה: "
-   , chats: "הודעה חדשה מ:"
-   , liveTalks: "שיחה חדשה: "
+  groups: "קבוצה חדשה: "
+  , topics: "נושא חדש: "
+  , questions: "שאלה חדשה: "
+  , owner: "קריאה: "
+  , chats: "הודעה חדשה מ:"
+  , liveTalks: "שיחה חדשה: "
 };
 var inactiveColor = "#5f1f1f";
 var activeColor = "white";
@@ -65,19 +65,19 @@ var activeColor = "white";
 
 // subsManager Def.
 var subsManager = {
-   isUpdatesSet: function(){
-      this.isFeedSet();
-      this.isNotificationsSet();
+  isUpdatesSet: function(){
+    this.isFeedSet();
+    this.isNotificationsSet();
 
-   },
-   setFeed: function(){},
-   isFeedSet: function(){},
-   setNotifications: function(){},
-   isNotificationsSet: function(){},
-   userFeed: null,
-   feedIsSet: false,
-   userNotifications: null,
-   notificationsIsSet: false
+  },
+  setFeed: function(){},
+  isFeedSet: function(){},
+  setNotifications: function(){},
+  isNotificationsSet: function(){},
+  userFeed: null,
+  feedIsSet: false,
+  userNotifications: null,
+  notificationsIsSet: false
 };
 
 var mostUpdatedContent = null;
@@ -88,25 +88,26 @@ const defaultFeedVolume = 10;
 
 // queue and inbox getters and setters defined in logic.js
 var feedManager = {
-   volume: defaultFeedVolume
+  volume: defaultFeedVolume
 };
 
 //==================================================================================
 
 const symbols = {
-   groups: "fa fa-users",
-   topics: "fa fa-folder-open",
-   questions: "fa fa-question-circle",
-   options: "fa fa-file-text",
-   liveTalks: "fa fa-eye"
+  groups: "fa fa-users",
+  topics: "fa fa-folder-open",
+  questions: "fa fa-question-circle",
+  options: "fa fa-file-text",
+  liveTalks: "fa fa-eye",
+  chats:"fa fa-comments-o"
 }
 
-//============// Initialize Firebase
-var config = {
-   apiKey: "AIzaSyAjyyjWM63PSjyRoDI-87MpRtfOFnOO0aA",
-   authDomain: "delib21-aaeb0.firebaseapp.com",
-   databaseURL: "https://delib21-aaeb0.firebaseio.com",
-   storageBucket: "delib21-aaeb0.appspot.com"
+  //============// Initialize Firebase
+  var config = {
+  apiKey: "AIzaSyAjyyjWM63PSjyRoDI-87MpRtfOFnOO0aA",
+  authDomain: "delib21-aaeb0.firebaseapp.com",
+  databaseURL: "https://delib21-aaeb0.firebaseio.com",
+  storageBucket: "delib21-aaeb0.appspot.com"
 };
 
 firebase.initializeApp(config);
